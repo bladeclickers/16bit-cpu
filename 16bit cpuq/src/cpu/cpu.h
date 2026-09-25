80457bb0cf6778c8401429c40e8411368666b7bc
r <120068330+bladeclickers@users.noreply.github.com>
2026-09-25 20:01:18 +1000

    new instructions + calculator

--- 16bit cpuq/src/cpu/cpu.h
+++ 16bit cpuq/src/cpu/cpu.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <cstdint>
+#include <cstdio>
 #include <iostream>
+#include <vector>
 #include "ram.h"
 
 struct data_s {
@@ -15,16 +17,24 @@ struct data_s {
 enum class instructions : uint8_t {
 	NOP = 0x00,
 	LDA = 0x01,
-	STA = 0x02,
-	ADD = 0x03,
-	SUB = 0x04,
-	MUL = 0x05,
-	DIV = 0x06,
-	JMP = 0x07,
-	JZ = 0x08,
-	JNZ = 0x09,
-	JC = 0x10,
-	JNC = 0x11,
+	LDI = 0x02,
+	STA = 0x03,
+	ADD = 0x04,
+	SUB = 0x05,
+	MUL = 0x06,
+	DIV = 0x07,
+	JMP = 0x08,
+	JZ = 0x09,
+	JNZ = 0x10,
+	JC = 0x11,
+	JNC = 0x12,
+	PUTC = 0x13,
+	GETC = 0x14,
+	PUTS = 0x15,
+	GETS = 0x16,
+	TOSTR = 0x17,
+	TOINT = 0x18,
+	CMP = 0x19,
 	HLT = 0xFF,
 	RET = 0xFE,
 };
@@ -52,6 +62,7 @@ void execute_instruction() {
 
 	bool has_operand = false;
 	switch (static_cast<instructions>(data.instr)) {
+	case instructions::LDI:
 	case instructions::LDA:
 	case instructions::STA:
 	case instructions::ADD:
@@ -63,11 +74,19 @@ void execute_instruction() {
 	case instructions::JNZ:
 	case instructions::JC:
 	case instructions::JNC:
+	case instructions::PUTC:
+	case instructions::GETC:
+	case instructions::PUTS:
+	case instructions::GETS:
+	case instructions::TOINT:
+	case instructions::TOSTR:
+	case instructions::CMP:
 		has_operand = true;
 		break;
 	default:
 		break;
 	}
+
 	if (has_operand) {
 		ram.mar = ram.memory[data.pcr];
 		data.pcr++;
@@ -75,6 +94,7 @@ void execute_instruction() {
 
 	switch (static_cast<instructions>(data.instr)) {
 	case instructions::LDA:  data.acr = ram.memory[ram.mar]; break;
+	case instructions::LDI:  data.acr = ram.mar; break;
 	case instructions::STA:  ram.memory[ram.mar] = data.acr; break;
 	case instructions::JMP:  data.pcr = ram.mar; break;
 	case instructions::JZ:   if (data.acr == 0) data.pcr = ram.mar; break;
@@ -105,6 +125,124 @@ void execute_instruction() {
 		data.flags = (b == 0 ? 0x01 : 0) | (((a >= 0 && b < 0 && data.acr < 0) || (a < 0 && b > 0 && data.acr > 0)) ? 0x02 : 0);
 		break;
 	}
+	case instructions::PUTC: {
+		putchar(static_cast<unsigned char>(ram.memory[ram.mar] & 0xFF));
+		data.flags = 0;
+    	break;
+	}
+	case instructions::GETC: {
+		int c = getchar();
+		if (c != EOF) ram.memory[ram.mar] = c;
+		data.flags = 0;
+		break;
+	}
+	case instructions::PUTS: {
+		int16_t a = ram.mar;
+
+		while (ram.memory[a] != 0) {
+			putchar(ram.memory[a] & 0xFF);
+			a++;
+		}
+
+		data.flags = 0;
+		break;
+	}
+	case instructions::GETS: {
+		int16_t a = data.acr + 1;
+		int16_t b = ram.mar;
+
+		std::vector<char> in(a);
+		std::cin.getline(in.data(), a);
+
+		for (int16_t i = 0; i < std::cin.gcount(); i++) {
+			ram.memory[b + i] = in[i];
+		}
+
+		ram.memory[b + std::cin.gcount()] = 0;
+
+		data.flags = 0;
+		break;
+	}
+	case instructions::TOSTR: {
+		int16_t value = data.acr;
+		int16_t address = ram.mar;
+
+		if (value == 0) {
+			ram.memory[address] = '0';
+			ram.memory[address + 1] = 0;
+			data.flags = 0;
+			break;
+		}
+
+		bool negative = value < 0;
+
+		uint16_t magnitude;
+		if (negative) {
+			magnitude = static_cast<uint16_t>(-(int32_t)value);
+		} else {
+			magnitude = static_cast<uint16_t>(value);
+		}
+
+		int16_t end = address;
+
+		while (magnitude > 0) {
+			ram.memory[end++] = '0' + (magnitude % 10);
+			magnitude /= 10;
+		}
+
+		if (negative) {
+			ram.memory[end++] = '-';
+		}
+
+		ram.memory[end] = 0;
+
+		int16_t left = address;
+		int16_t right = end - 1;
+
+		while (left < right) {
+			int16_t temp = ram.memory[left];
+			ram.memory[left] = ram.memory[right];
+			ram.memory[right] = temp;
+
+			left++;
+			right--;
+		}
+
+		data.flags = 0;
+		break;
+	}
+	case instructions::TOINT: {
+		int16_t address = ram.mar;
+		int16_t result = 0;
+		bool negative = false;
+
+		if (ram.memory[address] == '-') {
+			negative = true;
+			address++;
+		}
+
+		while (ram.memory[address] != 0) {
+			char c = static_cast<char>(ram.memory[address]);
+
+			if (c < '0' || c > '9')
+				break;
+
+			result = result * 10 + (c - '0');
+			address++;
+		}
+
+		data.acr = negative ? -result : result;
+		data.flags = 0;
+		break;
+	}
+	case instructions::CMP: {
+		int16_t a = data.acr;
+		int16_t b = ram.memory[ram.mar];
+
+		data.acr = a - b;
+		data.flags = (static_cast<uint16_t>(a) < static_cast<uint16_t>(b) ? 0x01 : 0) | (((a >= 0 && b < 0 && data.acr < 0) || (a < 0 && b > 0 && data.acr > 0)) ? 0x02 : 0);
+		break;
+	}
 	case instructions::HLT:
 	case instructions::RET:
 		break;
@@ -116,7 +254,7 @@ void get_result() {
 	bool carry = data.flags & 0x01;
 	bool zero = data.acr == 0;
 	bool negative = data.acr < 0;
-	bool overflow = data.flags & 0x0;
+	bool overflow = data.flags & 0x02;
 	bool parity = __builtin_popcount(data.acr) % 2 == 0; // msvc uses __popcnt
 	
 	std::cout << (carry ? "carry: true\n" : "");
--- 16bit cpuq/src/main.cpp
+++ 16bit cpuq/src/main.cpp
@@ -1,113 +1,94 @@
 #include <iostream>
 #include "cpu/cpu.h"
 
-constexpr uint16_t CELL_X = 0x1000;
+int16_t calculator[] = {
+	static_cast<int16_t>(instructions::PUTS), 93,
+	static_cast<int16_t>(instructions::LDI), 19,
+	static_cast<int16_t>(instructions::GETS), 400,
+	static_cast<int16_t>(instructions::TOINT), 400,
+	static_cast<int16_t>(instructions::STA), 450,
 
-int16_t add(const int16_t* numbers, size_t count) {
-	if (count == 0) return 0;
+	static_cast<int16_t>(instructions::PUTS), 114,
+	static_cast<int16_t>(instructions::LDI), 19,
+	static_cast<int16_t>(instructions::GETS), 420,
+	static_cast<int16_t>(instructions::TOINT), 420,
+	static_cast<int16_t>(instructions::STA), 451,
+
+	static_cast<int16_t>(instructions::PUTS), 136,
+	static_cast<int16_t>(instructions::GETC), 440,
+
+	static_cast<int16_t>(instructions::LDA), 440,
+	static_cast<int16_t>(instructions::CMP), 170,
+	static_cast<int16_t>(instructions::JZ), 49,
+
+	static_cast<int16_t>(instructions::LDA), 440,
+	static_cast<int16_t>(instructions::CMP), 171,
+	static_cast<int16_t>(instructions::JZ), 60,
+
+	static_cast<int16_t>(instructions::LDA), 440,
+	static_cast<int16_t>(instructions::CMP), 172,
+	static_cast<int16_t>(instructions::JZ), 71,
+
+	static_cast<int16_t>(instructions::LDA), 440,
+	static_cast<int16_t>(instructions::CMP), 173,
+	static_cast<int16_t>(instructions::JZ), 82,
+
+	static_cast<int16_t>(instructions::HLT),
+
+	static_cast<int16_t>(instructions::LDA), 450,
+	static_cast<int16_t>(instructions::ADD), 451,
+	static_cast<int16_t>(instructions::TOSTR), 460,
+	static_cast<int16_t>(instructions::PUTS), 161,
+	static_cast<int16_t>(instructions::PUTS), 460,
+	static_cast<int16_t>(instructions::HLT),
+
+	static_cast<int16_t>(instructions::LDA), 450,
+	static_cast<int16_t>(instructions::SUB), 451,
+	static_cast<int16_t>(instructions::TOSTR), 460,
+	static_cast<int16_t>(instructions::PUTS), 161,
+	static_cast<int16_t>(instructions::PUTS), 460,
+	static_cast<int16_t>(instructions::HLT),
+
+	static_cast<int16_t>(instructions::LDA), 450,
+	static_cast<int16_t>(instructions::MUL), 451,
+	static_cast<int16_t>(instructions::TOSTR), 460,
+	static_cast<int16_t>(instructions::PUTS), 161,
+	static_cast<int16_t>(instructions::PUTS), 460,
+	static_cast<int16_t>(instructions::HLT),
+
+	static_cast<int16_t>(instructions::LDA), 450,
+	static_cast<int16_t>(instructions::DIV), 451,
+	static_cast<int16_t>(instructions::TOSTR), 460,
+	static_cast<int16_t>(instructions::PUTS), 161,
+	static_cast<int16_t>(instructions::PUTS), 460,
+	static_cast<int16_t>(instructions::HLT),
+
+	'E', 'n', 't', 'e', 'r', ' ', 'f', 'i', 'r', 's', 't',
+	' ', 'n', 'u', 'm', 'b', 'e', 'r', ':', ' ', 0,
+
+	'E', 'n', 't', 'e', 'r', ' ', 's', 'e', 'c', 'o', 'n',
+	'd', ' ', 'n', 'u', 'm', 'b', 'e', 'r', ':', ' ', 0,
+
+	'O', 'p', 'e', 'r', 'a', 't', 'i', 'o', 'n', ' ', '(',
+	'+', ',', ' ', '-', ',', ' ', '*', ',', ' ', '/', ')',
+	':', ' ', 0,
+
+	'R', 'e', 's', 'u', 'l', 't', ':', ' ', 0,
+
+	'+', '-', '*', '/'
+};
+
+void run_calculator() {
 	reset();
-
-	size_t p = 0;
-	ram.insert(static_cast<int16_t>(instructions::LDA), p++);
-	ram.insert(static_cast<int16_t>(CELL_X), p++);
-
-	for (size_t i = 1; i < count; ++i) {
-		ram.insert(static_cast<int16_t>(instructions::ADD), p++);
-		ram.insert(static_cast<int16_t>(CELL_X + i), p++);
-	}
-
-	ram.insert(static_cast<int16_t>(instructions::HLT), p++);
-
-	for (size_t i = 0; i < count; ++i) {
-		ram.insert(static_cast<int16_t>(numbers[i]), CELL_X + i);
-	}
-
+	load_program(calculator, sizeof(calculator) / sizeof(calculator[0]));
 	run_program(false);
-	return data.acr;
-}
-
-int16_t sub(const int16_t* numbers, size_t count) {
-	if (count == 0) return 0;
-	reset();
-
-	size_t p = 0;
-	ram.insert(static_cast<int16_t>(instructions::LDA), p++);
-	ram.insert(static_cast<int16_t>(CELL_X), p++);
-
-	for (size_t i = 1; i < count; ++i) {
-		ram.insert(static_cast<int16_t>(instructions::SUB), p++);
-		ram.insert(static_cast<int16_t>(CELL_X + i), p++);
-	}
-
-	ram.insert(static_cast<int16_t>(instructions::HLT), p++);
-
-	for (size_t i = 0; i < count; ++i) {
-		ram.insert(static_cast<int16_t>(numbers[i]), CELL_X + i);
-	}
-
-	run_program(false);
-	return data.acr;
-}
-
-int16_t mul(const int16_t* numbers, size_t count) {
-	if (count == 0) return 0;
-	reset();
-
-	size_t p = 0;
-	ram.insert(static_cast<int16_t>(instructions::LDA), p++);
-	ram.insert(static_cast<int16_t>(CELL_X), p++);
-
-	for (size_t i = 1; i < count; ++i) {
-		ram.insert(static_cast<int16_t>(instructions::MUL), p++);
-		ram.insert(static_cast<int16_t>(CELL_X + i), p++);
-	}
-
-	ram.insert(static_cast<int16_t>(instructions::HLT), p++);
-
-	for (size_t i = 0; i < count; ++i) {
-		ram.insert(static_cast<int16_t>(numbers[i]), CELL_X + i);
-	}
-
-	run_program(false);
-	return data.acr;
-}
-
-int16_t divide(const int16_t* numbers, size_t count) {
-	if (count == 0) return 0;
-	for (size_t i = 1; i < count; ++i) {
-		if (numbers[i] == 0) return 0;
-	}
-	reset();
-
-	size_t p = 0;
-	ram.insert(static_cast<int16_t>(instructions::LDA), p++);
-	ram.insert(static_cast<int16_t>(CELL_X), p++);
-
-	for (size_t i = 1; i < count; ++i) {
-		ram.insert(static_cast<int16_t>(instructions::DIV), p++);
-		ram.insert(static_cast<int16_t>(CELL_X + i), p++);
-	}
-
-	ram.insert(static_cast<int16_t>(instructions::HLT), p++);
-
-	for (size_t i = 0; i < count; ++i) {
-		ram.insert(static_cast<int16_t>(numbers[i]), CELL_X + i);
-	}
-
-	run_program(false);
-	return data.acr;
 }
 
 int main()
 {
 	std::cout << std::dec;
 
-	int16_t input[] = { 20, 6 };
+	run_calculator();
 
-	std::cout << "add: " << add(input, sizeof(input) / sizeof(input[0])) << "\n";
-	std::cout << "sub: " << sub(input, sizeof(input) / sizeof(input[0])) << "\n";
-	std::cout << "mul: " << mul(input, sizeof(input) / sizeof(input[0])) << "\n";
-	std::cout << "div: " << divide(input, sizeof(input) / sizeof(input[0])) << "\n";
-	std::cin.get();
 	return 0;
 }
